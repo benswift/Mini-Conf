0b7378a99c4761f5473170cdc1c83992c5e1cf41
Ben Swift <ben@benswift.me>
2020-06-18 18:32:16 +1000

    getting close with the "turn all videos/audio into a single file script
    still some issues adding blank audio to the titlecards

--- scripts/prepend_title_cards.py
+++ scripts/prepend_title_cards.py
@@ -1,20 +1,27 @@
 import subprocess
+from pathlib import Path
+import re
 
 conference="ACMC 2020"
 typeface="AlegreyaSans" # use a font with Thin, Regular & Bold weights
+titlecard_length_sec = 3
 
-input_path = "."
-output_path = "."
+
+media_path = Path("media")
+output_path = media_path / "processed"
 
 def make_titlecard(artist, title, UID):
-    titlecard_path = f"{output_path}/{UID}-titlecard.mkv"
+    titlecard_path = media_path / "titlecards" / f"{UID}-titlecard.mkv"
     proc = subprocess.run(
         [
             "ffmpeg", "-y",
-            # select virtual input device
             "-f", "lavfi",
+            # add blank audio
+            "-i", "anullsrc", "-t", str(titlecard_length_sec),
             # set bg colour, video size & duration
-            "-i", "color=c=black:s=1920x1080:d=3",
+            "-f", "lavfi",
+            # select virtual input video device
+            "-i", f"color=c=black:s=1920x1080:d={titlecard_length_sec}",
             # title
             "-vf", f"drawtext=fontfile='{typeface}\:style=Thin':fontsize=160:fontcolor=#EEEEEE:x=100:y=h-500:text='{title}', " +
             # artist
@@ -30,17 +37,50 @@ def make_titlecard(artist, title, UID):
 
     return titlecard_path
 
-def process_video(artist, title, UID):
+def process_video():
 
-    titlecard_path = make_titlecard(artist, title, UID)
+    # accumulator variables
+    videos = []
+    filter_string = ""
+    video_index = 0
 
-    # now just smoosh them together
-    # TODO
+    # build up the list of files & filters for the ffmpeg command
+    for p in media_path.iterdir():
+        m = re.search('([0-9]+).(mp4|mkv|mov)', p.name)
+        if m:
+            videos.append("-i")
+            videos.append(make_titlecard("artist", "title", m.group(1)))
+            videos.append("-i")
+            videos.append(p)
+            filter_string = filter_string + f"[{video_index}:v] [{video_index}:a] [{video_index+1}:v] [{video_index+1}:a] "
+            video_index += 2
+
+    if not videos:
+        print("No valid videos found, aborting")
+        exit(1)
+
+    else:
+        filter_string = filter_string + f"concat=n={video_index}:v=1:a=1 [v] [a]"
+
+        print(filter_string)
+
+        # it's showtime!
+        subprocess.run(
+            ["ffmpeg"] + videos
+            + [
+                "-filter_complex",
+                filter_string,
+                "-map",
+                "[v]",
+                "-map",
+                "[a]",
+                "-y",
+                (output_path / "output.mkv").resolve(),
+            ]
+        )
+        return True
 
 
 if __name__ == '__main__':
-    artist="Ben Swift"
-    title="Untitled #3"
-    UID = 123
 
-    make_titlecard(artist, title, UID)
+    process_video()
