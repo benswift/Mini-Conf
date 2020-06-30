30c0c35371f9b7613c0984bb2b3b3bc464b05959
Ben Swift <ben@benswift.me>
2020-06-30 12:14:44 +1000

    add encoder args to make_video() as well

--- scripts/process_videos.py
+++ scripts/process_videos.py
@@ -220,6 +220,7 @@ def make_video(uid):
          "-i", titlecard_path,
          "-i", mp,
          "-filter_complex", "[1:v] setsar=sar=1/1, scale=1920:1080 [v]; [0:v] [0:a] [v] [1:a] concat=n=2:v=1:a=1",
+         *ffmpeg_encoder_args,
          of
         ]
     )
