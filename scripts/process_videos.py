13db3bd6a91538f538debf6b41a85fb63b55f4a2
Ben Swift <ben@benswift.me>
2020-07-02 13:43:33 +1000

    better handling of session_chair = None case for titlecards

--- scripts/process_videos.py
+++ scripts/process_videos.py
@@ -280,7 +280,11 @@ def make_session_titlecard(session_uid):
 
     output_path = tmp_path / f"{session_uid}-titlecard.mkv"
 
-    render_revealjs_index_html(s["title"], f"{s['date']} AEST", f"session chair: {s['chair']}")
+    chair = s['chair']
+    if chair:
+        render_revealjs_index_html(s["title"], f"{s['date']} AEST", f"session chair: {chair}")
+    else:
+        render_revealjs_index_html(s["title"], f"{s['date']} AEST")
 
     # ok, now run decktape to get the png
     proc = subprocess.run(
