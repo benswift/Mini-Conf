649753659a90cc95334453e0f4854f83e764da29
Ben Swift <ben@benswift.me>
2020-07-02 12:55:34 +1000

    use a 2M buffsize for nvenc

--- scripts/process_videos.py
+++ scripts/process_videos.py
@@ -9,7 +9,7 @@ from jinja2 import Template
 
 media_extensions = [".mkv", ".mov", ".mp4", ".avi", ".m4v", ".wav", ".aif", ".mp3"]
 # tweak as necessary for your platform, or use an empty list for defaults
-ffmpeg_encoder_args = ["-vcodec", "h264_nvenc", "-preset", "slow", "-b:v", "10M", "-maxrate", "10M", "-bufsize", "1M"]
+ffmpeg_encoder_args = ["-vcodec", "h264_nvenc", "-preset", "slow", "-b:v", "10M", "-maxrate", "10M", "-bufsize", "2M"]
 
 
 # paths - tweak `media_path` as necessary for your setup
