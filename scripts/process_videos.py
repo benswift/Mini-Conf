7f5ca3e4fddba5174bdef42afef3368fe3020eb0
Ben Swift <ben@benswift.me>
2020-07-05 11:56:49 +1000

    specify aac audio codec for anullsrc

--- scripts/process_videos.py
+++ scripts/process_videos.py
@@ -180,7 +180,7 @@ def make_titlecard(uid):
         [
             "ffmpeg", "-y",
             # titlecard png as an input source
-            "-f", "lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=48000", "-loop", "1", "-i", titlecard_path, "-t", "10", "-c:v", "copy", "-shortest",
+            "-f", "lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=48000", "-loop", "1", "-i", titlecard_path, "-t", "10", "-c:a", "aac", "-c:v", "copy", "-shortest",
             *ffmpeg_encoder_args,
             output_path
         ]
